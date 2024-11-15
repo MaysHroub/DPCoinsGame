d65f4eff7a21be0819062adea4cc6b586189adff
Mays-Alreem <hroubmays@gmail.com>
2024-11-15 22:11:07 +0200

    feat: set action for next button

--- src/main/java/com/msreem/dpcoinsgame/panes/ManualInputPane.java
+++ src/main/java/com/msreem/dpcoinsgame/panes/ManualInputPane.java
@@ -12,17 +12,21 @@ import javafx.scene.layout.VBox;
 
 public class ManualInputPane extends BorderPane {
 
+    private Label errorLabel;
+
+
     public ManualInputPane() {
         init();
     }
 
     private void init() {
         Label promptLabel = new Label("Enter a group of even number of coins\n(comma separated and up to 20 coins)");
-        Label errorLabel = new Label();
+        errorLabel = new Label();
         TextField inputTF = new TextField();
         inputTF.setPromptText("Eg. 4,15,7,3,8,9");
 
         Button nextBtn = new Button("NEXT");
+        nextBtn.setOnAction(e -> validateInput(inputTF.getText()));
 
         VBox vBox = new VBox(40, promptLabel, inputTF, errorLabel);
         vBox.setAlignment(Pos.CENTER);
@@ -33,6 +37,25 @@ public class ManualInputPane extends BorderPane {
         BorderPane.setAlignment(nextBtn, Pos.CENTER);
 
         BorderPane.setMargin(nextBtn, new Insets(0, 0, 50, 0));
+    }
+
+    private int[] validateInput(String input) {
+        if (input == null) return null;
+        String[] tokens = input.split(",");
+        if (tokens.length == 0 || tokens.length % 2 != 0) {
+            errorLabel.setText("Please enter even number of coins and comma separated :)");
+            return null;
+        }
+        int[] coins = new int[tokens.length];
+        for (int i = 0; i < tokens.length; i++) {
+            try {
+                coins[i] = Integer.parseInt(tokens[i]);
+            } catch (NumberFormatException e) {
+                errorLabel.setText("Please enter only integers :/");
+                return null;
+            }
+        }
+        return coins;
     }
 
 }
