98605f43a3bd628f261459c3c8f3a514c5be0af7
Mays-Alreem <hroubmays@gmail.com>
2024-11-15 21:35:56 +0200

    Âfeat: create ManualInputPane class

---
+++ src/main/java/com/msreem/dpcoinsgame/panes/ManualInputPane.java
@@ -0,0 +1,15 @@
+package com.msreem.dpcoinsgame.panes;
+
+import javafx.scene.layout.BorderPane;
+
+public class ManualInputPane extends BorderPane {
+
+    public ManualInputPane() {
+        init();
+    }
+
+    private void init() {
+
+    }
+
+}
