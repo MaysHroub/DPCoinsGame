ad17a559569273482cf87c61a3d57d016b140981
Mays-Alreem <hroubmays@gmail.com>
2024-11-15 20:53:53 +0200

    feat: init input-pane with ui controls

--- src/main/java/com/msreem/dpcoinsgame/panes/InputPane.java
+++ src/main/java/com/msreem/dpcoinsgame/panes/InputPane.java
@@ -1,18 +1,43 @@
 package com.msreem.dpcoinsgame.panes;
 
+import com.msreem.dpcoinsgame.util.Animation;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 public class InputPane extends BorderPane {
 
-    private Button readFromFileBtn, enterManuallyBtn, generateRandomBtn;
+    private Button loadFromFileBtn, enterManuallyBtn, generateRandomBtn;
 
     public InputPane() {
         init();
     }
 
     private void init() {
+        loadFromFileBtn = new Button("Load data from file");
+        enterManuallyBtn = new Button("Enter data manually");
+        generateRandomBtn = new Button("Generate random data");
 
+        VBox buttonsVB = new VBox(40, loadFromFileBtn, enterManuallyBtn, generateRandomBtn);
+        buttonsVB.setAlignment(Pos.BOTTOM_CENTER);
+
+        Label label = new Label("SELECT YOUR INPUT METHOD");
+
+        setTop(label);
+        setCenter(buttonsVB);
+
+        BorderPane.setAlignment(label, Pos.CENTER);
+
+        BorderPane.setMargin(buttonsVB, new Insets(0, 0, 100, 0));
+        BorderPane.setMargin(label, new Insets(100, 0, 0, 0));
+
+        Animation.installFadeTransition(label, 1);
+        Animation.installTranslateYTransition(loadFromFileBtn, 1, loadFromFileBtn.getTranslateY()+200, loadFromFileBtn.getTranslateY());
+        Animation.installTranslateYTransition(enterManuallyBtn, 1.2, loadFromFileBtn.getTranslateY()+150, loadFromFileBtn.getTranslateY());
+        Animation.installTranslateYTransition(generateRandomBtn, 1.4, loadFromFileBtn.getTranslateY()+100, loadFromFileBtn.getTranslateY());
     }
 
 }
