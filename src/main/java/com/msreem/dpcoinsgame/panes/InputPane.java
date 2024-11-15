b5912ca44e03a54834666137bca7c924da0e44a6
Mays-Alreem <hroubmays@gmail.com>
2024-11-15 20:36:31 +0200

    feat: create input-pane class

---
+++ src/main/java/com/msreem/dpcoinsgame/panes/InputPane.java
@@ -0,0 +1,18 @@
+package com.msreem.dpcoinsgame.panes;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
+public class InputPane extends BorderPane {
+
+    private Button readFromFileBtn, enterManuallyBtn, generateRandomBtn;
+
+    public InputPane() {
+        init();
+    }
+
+    private void init() {
+
+    }
+
+}
