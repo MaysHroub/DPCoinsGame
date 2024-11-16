d41d1e08836efcce57dc7f8204b579481463cec3
Mays-Alreem <hroubmays@gmail.com>
2024-11-16 21:05:06 +0200

    feat: create page for two players' names

--- src/main/java/com/msreem/dpcoinsgame/HelloApplication.java
+++ src/main/java/com/msreem/dpcoinsgame/HelloApplication.java
@@ -2,6 +2,7 @@ package com.msreem.dpcoinsgame;
 
 import com.msreem.dpcoinsgame.panes.ManualInputPane;
 import com.msreem.dpcoinsgame.panes.RandomInputPane;
+import com.msreem.dpcoinsgame.panes.TwoPlayerInfoPane;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -9,7 +10,7 @@ import javafx.stage.Stage;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) {
-        Scene scene = new Scene(new RandomInputPane(), 1200, 700);
+        Scene scene = new Scene(new TwoPlayerInfoPane(), 1200, 700);
         scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/msreem/dpcoinsgame/panes/TwoPlayerInfoPane.java
+++ src/main/java/com/msreem/dpcoinsgame/panes/TwoPlayerInfoPane.java
@@ -1,11 +1,52 @@
 package com.msreem.dpcoinsgame.panes;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class TwoPlayerInfoPane extends BorderPane {
 
-    private void init() {
+    public TwoPlayerInfoPane() {
+        init();
+    }
 
+    private void init() {
+        Label promptName1Label = new Label("Enter Player 1 Name"),
+                promptName2Label = new Label("Enter Player 2 Name"),
+                alertLabel = new Label();
+        ImageView marioIV = new ImageView("C:\\Users\\ismae\\IdeaProjects\\DPCoinsGame\\src\\main\\resources\\images\\mario.png"),
+                luigiIV = new ImageView("C:\\Users\\ismae\\IdeaProjects\\DPCoinsGame\\src\\main\\resources\\images\\luigi.png");
+        TextField nameOneTF = new TextField(),
+                    nameTwoTF = new TextField();
+        Button nextBtn = new Button("NEXT");
+
+        marioIV.setPreserveRatio(true);
+        marioIV.setFitHeight(250);
+        luigiIV.setPreserveRatio(true);
+        luigiIV.setFitHeight(250);
+
+        VBox playerOneVB = new VBox(20, marioIV, promptName1Label, nameOneTF);
+        VBox playerTwoVB = new VBox(20, luigiIV, promptName2Label, nameTwoTF);
+
+        playerOneVB.setAlignment(Pos.CENTER);
+        playerTwoVB.setAlignment(Pos.CENTER);
+
+        HBox hBox = new HBox(100, playerOneVB, playerTwoVB);
+        hBox.setAlignment(Pos.CENTER);
+
+        VBox vBox = new VBox(20, alertLabel, nextBtn);
+        vBox.setAlignment(Pos.CENTER);
+
+        setCenter(hBox);
+        setBottom(vBox);
+
+        setPadding(new Insets(20, 20, 50, 20));
     }
 
 }
