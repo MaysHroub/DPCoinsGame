8c2caf3c4da07360eaa11b287d1aa697c2a8ffa2
Mays-Alreem <hroubmays@gmail.com>
2024-11-17 22:40:51 +0200

    fix: modify coin style

--- src/main/java/com/msreem/dpcoinsgame/panes/Coin.java
+++ src/main/java/com/msreem/dpcoinsgame/panes/Coin.java
@@ -1,5 +1,6 @@
 package com.msreem.dpcoinsgame.panes;
 
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -8,10 +9,10 @@ import javafx.scene.shape.Circle;
 public class Coin extends StackPane {
 
     public Coin(int value) {
-        Circle circle = new Circle(23);
-        circle.setStyle("-fx-fill: gold; -fx-stroke: orange; -fx-stroke-width: 3;");
+        Circle circle = new Circle(22);
+        circle.setStyle("-fx-fill: gold; -fx-stroke: orange; -fx-stroke-width: 3; -fx-text-alignment: center;");
         Label label = new Label(value + "");
-        label.setStyle("-fx-font-size: 14;");
+        label.setStyle("-fx-font-size: 12;");
         getChildren().addAll(circle, label);
     }
 
