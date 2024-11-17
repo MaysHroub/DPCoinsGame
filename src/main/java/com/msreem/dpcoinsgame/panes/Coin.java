24d93689904e2f9e052197ea587948684d6ce5c0
Mays-Alreem <hroubmays@gmail.com>
2024-11-17 22:25:37 +0200

    style: add css style to coins

--- src/main/java/com/msreem/dpcoinsgame/panes/Coin.java
+++ src/main/java/com/msreem/dpcoinsgame/panes/Coin.java
@@ -8,8 +8,10 @@ import javafx.scene.shape.Circle;
 public class Coin extends StackPane {
 
     public Coin(int value) {
-        Circle circle = new Circle(23, Color.GOLD);
+        Circle circle = new Circle(23);
+        circle.setStyle("-fx-fill: gold; -fx-stroke: orange; -fx-stroke-width: 3;");
         Label label = new Label(value + "");
+        label.setStyle("-fx-font-size: 14;");
         getChildren().addAll(circle, label);
     }
 
