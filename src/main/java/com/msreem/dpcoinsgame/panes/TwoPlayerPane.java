1064b91a28f7755401e36f01630c557510876adf
Mays-Alreem <hroubmays@gmail.com>
2024-11-17 20:20:22 +0200

    feat: create upper part of the page

--- src/main/java/com/msreem/dpcoinsgame/panes/TwoPlayerPane.java
+++ src/main/java/com/msreem/dpcoinsgame/panes/TwoPlayerPane.java
@@ -13,7 +13,24 @@ public class TwoPlayerPane extends StackPane {
     }
 
     private void init() {
+        Label playerOneName = new Label("MARIO"),
+                playerTwoName = new Label("LUIGI"),
+                playerTurn = new Label("Mario's Turn");
+        ImageView marioImg = new ImageView("C:\\Users\\ismae\\IdeaProjects\\DPCoinsGame\\src\\main\\resources\\images\\mario_fight.png"),
+                luigiImg = new ImageView("C:\\Users\\ismae\\IdeaProjects\\DPCoinsGame\\src\\main\\resources\\images\\luigi_fight.png");
 
+        marioImg.setFitHeight(200);
+        marioImg.setPreserveRatio(true);
+        luigiImg.setFitHeight(200);
+        luigiImg.setPreserveRatio(true);
+
+        VBox playerOneVB = new VBox(20, playerOneName, marioImg),
+                playerTwoVB = new VBox(20, playerTwoName, luigiImg);
+
+        BorderPane upperLayout = new BorderPane();
+        upperLayout.setCenter(playerTurn);
+        upperLayout.setLeft(playerOneVB);
+        upperLayout.setRight(playerTwoVB);
     }
 
 }
