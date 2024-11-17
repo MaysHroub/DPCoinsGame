b9ae8274db2dbd2fbff717b04341badb83b70ccf
Mays-Alreem <hroubmays@gmail.com>
2024-11-17 20:40:55 +0200

    feat: create coins pane

--- src/main/java/com/msreem/dpcoinsgame/panes/TwoPlayerPane.java
+++ src/main/java/com/msreem/dpcoinsgame/panes/TwoPlayerPane.java
@@ -3,8 +3,11 @@ package com.msreem.dpcoinsgame.panes;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+
+import java.util.Arrays;
 
 public class TwoPlayerPane extends StackPane {
 
@@ -31,6 +34,13 @@ public class TwoPlayerPane extends StackPane {
         upperLayout.setCenter(playerTurn);
         upperLayout.setLeft(playerOneVB);
         upperLayout.setRight(playerTwoVB);
+
+        Coin[] coins = new Coin[20];
+        for (int i = 0; i < 20; i++)
+            coins[i] = new Coin(i+1);
+
+        HBox coinsHB = new HBox(10);
+        coinsHB.getChildren().addAll(Arrays.asList(coins));
     }
 
 }
