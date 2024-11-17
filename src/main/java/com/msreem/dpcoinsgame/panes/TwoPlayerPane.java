3c9602f735c487d93c4b3c41d5d62b53c9652998
Mays-Alreem <hroubmays@gmail.com>
2024-11-17 20:19:33 +0200

    feat: create class for two player page

---
+++ src/main/java/com/msreem/dpcoinsgame/panes/TwoPlayerPane.java
@@ -0,0 +1,19 @@
+package com.msreem.dpcoinsgame.panes;
+
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+public class TwoPlayerPane extends StackPane {
+
+    public TwoPlayerPane() {
+        init();
+    }
+
+    private void init() {
+
+    }
+
+}
