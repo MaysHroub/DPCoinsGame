1925bc28aea14a04755e732b55f8af1f9a985d11
Mays-Alreem <hroubmays@gmail.com>
2024-11-20 22:27:13 +0200

    Ârefactor: modify NavigationManager class

--- src/main/java/com/msreem/dpcoinsgame/navigation/NavigationManager.java
+++ src/main/java/com/msreem/dpcoinsgame/navigation/NavigationManager.java
@@ -1,22 +1,33 @@
 package com.msreem.dpcoinsgame.navigation;
 
+import com.msreem.dpcoinsgame.panetag.PaneTag;
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class NavigationManager {
 
     private static NavigationManager instance;
-    // TO-DO: implement your own arraylist class
-    private ArrayList<Pane> panes;
+    private Scene scene;
 
     private NavigationManager() {}
-
 
     public static NavigationManager getInstance() {
         if (instance == null)
             instance = new NavigationManager();
         return instance;
+    }
+
+    public void init(Scene scene) {
+        this.scene = scene;
+    }
+
+    public void navigateTo(PaneTag tag) {
+        switch (tag) {
+
+        }
     }
 
 }
