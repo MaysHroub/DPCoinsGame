80307e3bec579bf135b92cad28a520da5dd666aa
Mays-Alreem <hroubmays@gmail.com>
2024-11-21 08:47:50 +0200

    feat: add tags for panes

--- src/main/java/com/msreem/dpcoinsgame/panetag/PaneTag.java
+++ src/main/java/com/msreem/dpcoinsgame/panetag/PaneTag.java
@@ -1,4 +1,11 @@
 package com.msreem.dpcoinsgame.panetag;
 
 public enum PaneTag {
+    START,
+    INPUT_OPTIONS,
+    USER_INPUT,
+    RANDOM_INPUT,
+    PLAYERS_NAME_INPUT,
+    PLAYERS_GAME,
+    DP_GAME
 }
