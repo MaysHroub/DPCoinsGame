70239e1b469dadac4e1dfbc364717c565d679111
Mays-Alreem <hroubmays@gmail.com>
2024-12-10 11:32:10 +0200

    feat: update dp logic methods

--- src/main/java/com/msreem/dpcoinsgame/dp/DPGameLogic.java
+++ src/main/java/com/msreem/dpcoinsgame/dp/DPGameLogic.java
@@ -18,31 +18,46 @@ public class DPGameLogic {
         dpTable = new int[n][n];
         directions = new char[n][n];
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
+            // calculating cumulative sum
             cum[i+1] = cum[i] + coins[i];
 
-        for (int i = 0; i < n; i++)
+            // setting initial value
             dpTable[i][i] = coins[i];
+            if (i > 0) {
+                int lowerVal = dpTable[i][i], leftVal = dpTable[i-1][i-1];
+                if (leftVal > lowerVal) {
+                    dpTable[i-1][i] = leftVal;
+                    directions[i-1][i] = 'D';
+                } else {
+                    dpTable[i-1][i] = lowerVal;
+                    directions[i-1][i] = 'L';
+                }
+            }
+        }
 
-        for (int k = 0; k < n; k++)
-            for (int i = 0, j = k+1; j < n; i++, j++) {
-                int sum1 = coins[i] + cum[j+1]-cum[i+1] - dpTable[i+1][j],
-                        sum2 = coins[j] + cum[j]-cum[i] - dpTable[i][j-1];
-                dpTable[i][j] = Math.max(sum1, sum2);
-                if (sum1 > sum2)
-                    directions[i][j] = 'D';
-                else if (sum1 < sum2)
-                    directions[i][j] = 'L';
-                else {
-                    if (coins[i] > coins[j])
-                        directions[i][j] = 'D';
+        for (int k = 2; k <= n-1; k++)
+            for (int i = 0; i+k < n; i++) {
+                int sum1 = coins[i] + cum[i+k+1]-cum[i+1] - dpTable[i+1][i+k],
+                        sum2 = coins[i+k] + cum[i+k]-cum[i] - dpTable[i][i+k-1];
+                dpTable[i][i+k] = Math.max(sum1, sum2);
+
+                if (sum1 > sum2) // by picking first coin
+                    directions[i][i+k] = 'D';  // go down
+
+                else if (sum1 < sum2) // by picking last coin
+                    directions[i][i+k] = 'L';  // go left
+
+                else { // sum1 = sum2
+                    if (coins[i] > coins[i+k])
+                        directions[i][i+k] = 'D';
                     else
-                        directions[i][j] = 'L';
+                        directions[i][i+k] = 'L';
                 }
             }
     }
 
-    // Identifies coins selected by each player.
+    // Identifies coins' indices selected by each player.
     public void calculatePlayersCoins() {
         int n = coins.length;
         playerOneCoinIndices = new int[n/2];
@@ -51,10 +66,10 @@ public class DPGameLogic {
         int l = 0, r = n-1;
         while (l <= r) {
             char dir = directions[l][r];
-            if ((r-l) % 2 != 0) {
+            if ((r-l) % 2 != 0) { // playerOne's turn
                 playerOneCoinIndices[i1] = dir == 'L' ? r : l;
                 i1++;
-            } else {
+            } else { // playerTwo's turn
                 playerTwoCoinIndices[i2] = dir == 'L' ? r : l;
                 i2++;
             }
